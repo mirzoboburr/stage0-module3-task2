e91a5fcc8767f74ec6caa3f5eaea73f0da9b4a80
Bobur <bobur927788575@outlook.com>
2022-11-24 21:00:45 +0500

    j

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -3,12 +3,12 @@ package lang.print.gaps.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
-        int variable = 2000;
-        int value = 1000;
-        int variable = 4000;
-        int value2 = 3000;
-        int variables = 5000;
-        variables = 7000;
-        int value = 90000;
+        int two = 2000;
+        int one = 1000;
+        int four = 4000;
+        int three = 3000;
+        int five = 5000;
+        int seven = 7000;
+        int nine = 90000;
     }
 }
